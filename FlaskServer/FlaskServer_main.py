bf4a2d6353e8ec59c5a8593d0fa90b32fcc9390c
Taehoon <lth9029@gmail.com>
2024-10-18 16:01:31 +0900

    FEAT:.env 추가로 인한 코드 수정

--- FlaskServer/FlaskServer_main.py
+++ FlaskServer/FlaskServer_main.py
@@ -16,49 +16,36 @@ import os
 from dotenv import load_dotenv
 
 # 프로젝트 루트 디렉토리를 sys.path에 추가
-# sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(project_root)
 load_dotenv()
 
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://keti_root:madcoder@bigsoft.iptime.org:55411/KETI_IISRC_Timescale'   #개발용
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://keti_root:madcoder@172.24.0.2:5432/KETI_IISRC_Timescale'              #배포용
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('POSTGRES_URI')
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('POSTGRES_URI_DEV')       #개발용
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('POSTGRES_URI_DEPLOY')    #배포용
+
+
 db = SQLAlchemy(app)
 app.logger.setLevel(logging.DEBUG)
 
 celery = Celery('FlaskServer_main', 
-            # broker='redis://bigsoft.iptime.org:55419/0',      #개발용
-            # backend='redis://bigsoft.iptime.org:55419/1'      #개발용
-            # broker='redis://172.24.0.4:6379/0',                 #배포용
-            # backend='redis://172.24.0.4:6379/1'                 #배포용
-            broker=os.getenv('CELERY_BROKER'),
-            backend=os.getenv('CELERY_BACKEND')
-        )
+            # broker=os.getenv('CELERY_BROKER_DEV'),        #개발용
+            # backend=os.getenv('CELERY_BACKEND_DEV')       #개발용
+            broker=os.getenv('CELERY_BROKER_DEPLOY'),       #배포용
+            backend=os.getenv('CELERY_BACKEND_DEPLOY')      #배포용
+)
 # Celery 로거 생성
 celery_logger = get_task_logger(__name__)
 
 
 # TimescaleDB 연결 설정 (커넥션 풀 사용 권장)
 def get_ts_conn():
-    # return psycopg2.connect(
-    #     # host="bigsoft.iptime.org",        #개발용
-    #     # port="55411",                     #개발용
-    #     host="172.24.0.2",                  #배포용
-    #     port="5432",                        #배포용
-    #     database="KETI_IISRC_Timescale",
-    #     user="keti_root",
-    #     password="madcoder",
-    # )
     return psycopg2.connect(app.config['SQLALCHEMY_DATABASE_URI'])
-
 
 def get_mongo_client():
     """각 워커에서 MongoClient를 생성하는 함수"""
-    # return MongoClient("mongodb://keti_root:madcoder@bigsoft.iptime.org:55410/")      #개발용
-    # return MongoClient("mongodb://keti_root:madcoder@172.24.0.3:27017/")                #배포용
-    mongo_uri = os.getenv('MONGO_URI')
+    mongo_uri = os.getenv('MONGO_URI_DEV')      #개발용
+    mongo_uri = os.getenv('MONGO_URI_DEPLOY')   #배포용
     return MongoClient(mongo_uri)
 
 
@@ -123,9 +110,7 @@ class InputSensorData(Resource):
         data = request.json
         # MongoDB에 먼저 데이터 저장
         try:
-            mongo_collection.insert_one(data)
-            # Celery 비동기 작업을 호출
-            # transfer_data.delay()            
+            mongo_collection.insert_one(data)   
 
             return {"message": "Sensor log saved to MongoDB and processing"}, 201
         except Exception as e:
@@ -264,8 +249,8 @@ log_query_type_parser.add_argument('start_time', required=True, type=str, help='
 log_query_type_parser.add_argument('end_time', required=True, type=str, help='End time in ISO 8601 format (e.g., 2024-01-02 or 2024-01-01T23:59)')
 log_query_type_parser.add_argument('sensor_type', required=True, type=str, help='Sensor type to filter (e.g., temperature, humidity)')
 
-@ns_sensor.route('/GetLogsByType')
-class GetLogsByType(Resource):
+@ns_sensor.route('/GetLogsBySensorType')
+class GetLogsBySensorType(Resource):
     @ns_sensor.expect(log_query_type_parser)
     @ns_sensor.response(200, 'Success', fields.List(fields.Raw))
     @ns_sensor.response(400, 'Missing required parameters')
